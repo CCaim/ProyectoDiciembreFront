0d735896fba6d64ac9bce077f24517f80e82f262
CCaim <barreirocarlos2001@gmail.com>
2024-10-08 19:17:16 +0200

    Se crea y completa genero-component

--- src/app/components/admin/admin-generos/admin-generos.component.ts
+++ src/app/components/admin/admin-generos/admin-generos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Genero, GeneroService } from '../../../service/genero.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-generos',
@@ -9,4 +11,71 @@ import { Component } from '@angular/core';
 })
 export class AdminGenerosComponent {
 
+  generos: Genero[] = []
+  generoNew: Genero ={
+
+  };
+
+  generoAEditar: Genero = {};
+
+  constructor(private _generoService:GeneroService){
+
+  }
+
+  ngOnInit(): void {
+    this.actualizarTabla();
+    
+  }
+
+  actualizarTabla(){
+    this._generoService.getGeneros().subscribe(
+      (response)=>{
+        this.generos = response;
+      }
+    );
+  }
+
+  buscarGeneroAEditar(id:number){
+    let generoEditar = this.generos.find(objeto => objeto.id === id);
+    if(generoEditar){
+      this.generoAEditar = generoEditar;
+    }
+  }
+
+  saveGenero(form: NgForm){
+    if(form.valid){
+      console.log(this.generoNew);
+      this._generoService.createGenero(this.generoNew).subscribe(
+        (response)=>{
+          console.log(response);
+          window.location.reload();
+        }
+      );
+    } else{
+      console.log('Fallo en save Genero');
+    }
+  }
+
+  editarGenero(form: NgForm){
+    if(form.valid){
+      console.log(this.generoAEditar);
+      this._generoService.putGenero(this.generoAEditar.id!, this.generoAEditar).subscribe(
+        (response)=>{
+          console.log(response);
+          window.location.reload();
+        }
+      );
+    } else {
+      console.log('error en editar genero');
+    }
+  }
+
+  eliminarGenero(id: number){
+    this._generoService.removeGenero(id).subscribe(
+      (response)=>{
+        console.log(response);
+        this.actualizarTabla();
+      }
+    )
+  }
 }
